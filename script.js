7c147085599a049e774ca4e41dde0260600c0510
Dmitriybodlev <dmitriybodlev@lwo-vl0020-153.corp.dtm.io>
2017-07-20 15:23:08 +0300

    first commit

--- script.js
+++ script.js
@@ -1,77 +1,3 @@
-function Pokemon() {
-
-}
-Pokemon.prototype.getType = function () {
-    return this.type;
-}
-Pokemon.prototype.levelUp = function () {
-    this.level += 1;
-    if (this.level === this.evolutionRule || this.level >= this.evolutionRule) {
-        console.log('evolved');
-        this.evolve()
-    }
-    return this.level;
-}
-Pokemon.prototype.evolve = function (height, weight, stone = null) {
-    let next;
-    // if(this.stone) {
-
-    // }
-    // if(this.nextEvolutonStep instanceof Object) {
-    //     if(this.nextEvolutonStep[stone]) {
-    //         next = this.nextEvolutonStep[stone];
-    //     }
-    // }
-    newEvolutionStep = new window[this.nextEvolutonStep](2, 30, this.level);
-    this.__proto__ = Object.create(window[this.nextEvolutonStep].prototype);
-    Object.keys(newEvolutionStep).map(key => {
-        this[key] = newEvolutionStep[key];
-    })
-}
-Pokemon.prototype.canWalk = function () {
-    if (this.walk === true) {
-        return true;
-    }
-    return false;
-}
-Pokemon.prototype.canFly = function () {
-    if (this.fly === true) {
-        return true;
-    }
-    return false;
-}
-
-// function for creating Constructors
-function createPokemonConstructor(constructorName, type, values = {}, nextEvolutonStep = null, evolutionRule = null) {
-    window[constructorName] = function (height, weight, level) {
-        this.pokemonType = constructorName;
-        this.type = type;
-        this.height = height;
-        this.weight = weight;
-        this.level = level;
-        this.nextEvolutonStep = nextEvolutonStep;
-        this.evolutionRule = evolutionRule;
-        Object.keys(values).map(key => {
-            this[key] = values[key];
-        })
-    }
-    window[constructorName].prototype = Object.create(Pokemon.prototype);
-    window[constructorName].constructor = window[constructorName];
-}
-
-createPokemonConstructor('Charmander', 'fire', { walk: true }, 'Charmeleon', 16);
-createPokemonConstructor('Charmeleon', 'fire', { walk: true }, 'Charizard', 36);
-Charmeleon.prototype.fire = function () {
-    console.log('fire');
-}
-createPokemonConstructor('Charizard', 'fire', { walk: true, fly: true });
-
-let fireName = new Charmander(1, 20, 1);
-let fireName2 = new Charmeleon(2, 30, 16);
-let fireName3 = new Charizard(5, 100, 36);
-
-
-
 let number;
 let defaultNumber = 10;
 let mines;
@@ -81,23 +7,22 @@ let newnewArr;
 let gameEnded = false;
 let shiftPushed = false;
 
+function setMapSize() {
+    let promptVal = prompt('Set size of map in format number*number\nInput number: ', 10);
+    number = promptVal === null || promptVal === '' ? defaultNumber : promptVal;
+    if (isNaN(number) || number > 30) {
+        setMapSize();
+    }
+    number = parseInt(number, 10);
+}
+
 function setMines() {
-    let promptVal = prompt('Set number for mines: ', 20);
-    console.log(promptVal)
+    let promptVal = prompt('Set number for mines: ', (number * number) * 0.1);
     mines = promptVal === null || promptVal === '' ? defaultMines : promptVal;
     if (mines > number * number || mines < (number * number) * 0.1 || isNaN(mines)) {
         setMines();
     }
     mines = parseInt(mines, 10);
-}
-
-function setMapSize() {
-    let promptVal = prompt('Set size of map in format number*number\nInput number: ', 10);
-    number = promptVal === null || promptVal === '' ? defaultNumber : promptVal;
-    if (isNaN(number) || number > 80) {
-        setMapSize();
-    }
-    number = parseInt(number, 10);
 }
 
 function setShiftPushed(e, pushed) {
@@ -137,7 +62,6 @@ function startGame() {
 
 startGame();
 
-
 function valuePlus(index) {
     if (newArr[index] === '*') return;
     newArr[index] += 1;
@@ -176,7 +100,7 @@ function renderMap() {
         newnewArr[i] = newArr.slice(number * i, (i + 1) * number);
     }
 
-    document.getElementById('main').innerHTML = '';
+    document.getElementById('game').innerHTML = '';
     newnewArr.forEach(item => {
         let div = document.createElement('div');
         let string = '';
@@ -186,7 +110,7 @@ function renderMap() {
             string += tag + 'value="' + it.id + '" ' + 'onClick="some(this)">' + it.content + insertedTag + '"></span></span>';
         })
         div.innerHTML = string;
-        document.getElementById('main').appendChild(div);
+        document.getElementById('game').appendChild(div);
     })
 
 }
@@ -203,9 +127,6 @@ function openAllMines() {
 
 function checkIfWin() {
     return !newArr.some(item => {
-        if ((item.content !== '*' && item.isOpened === false)) {
-            console.log(item)
-        }
         return (item.content !== '*' && item.isOpened === false);
     })
 }
@@ -257,8 +178,6 @@ function some(e) {
         }, 0)
     }
 
-
-    console.log(checkIfWin())
     if (checkIfWin()) {
         openAllMines();
         setTimeout(() => {
@@ -271,14 +190,3 @@ function some(e) {
     }
 
 }
-// console.log(string);
-
-
-// let descArr = [['*',1,0],[1,0,0],[0,0,0]];
-// descArr.forEach(item => {
-//     let string = '';
-//     item.forEach(it => {
-//         string += it + ' ';
-//     })
-//     console.log(string);
-// })
